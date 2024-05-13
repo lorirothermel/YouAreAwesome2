ec0d32b9098c510ac8b9295fcb72e2a0afcf0c40
lorirothermel <lori@rothermel1.com>
2024-05-12 23:35:05 -0400

    GeoReader and Divider

--- YouAreAwesome2/ContentView.swift
+++ YouAreAwesome2/ContentView.swift
@@ -12,51 +12,63 @@ struct ContentView: View {
     
     var body: some View {
         
-        VStack {
-            
-//            Text("You Have Skills!")
-//                .font(.largeTitle)
-//                .fontWeight(.black)
-//                .foregroundColor(Color("Gold-BC"))
-//                .padding()
-//                .background(Color("Maroon-BC"))
-//                .padding(1)
-//                .cornerRadius(15)
-            
-            Spacer()
-            
-            Text(messageString)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .minimumScaleFactor(0.5)
-                .multilineTextAlignment(.center)
-                .foregroundColor(.red)
-                .frame(height: 150)
-                .frame(maxWidth: .infinity)
-                .border(.orange, width: 5)
-                .padding()
-            
-            Spacer()
-                       
-            HStack {
-            
-                Button("Awesome") {
-                    messageString = "You Are Awesome!"
-                }  // Button
-                .buttonStyle(.borderedProminent)
+        GeometryReader { geometry in
+            VStack {
+                
+    //            Text("You Have Skills!")
+    //                .font(.largeTitle)
+    //                .fontWeight(.black)
+    //                .foregroundColor(Color("Gold-BC"))
+    //                .padding()
+    //                .background(Color("Maroon-BC"))
+    //                .padding(1)
+    //                .cornerRadius(15)
                 
                 Spacer()
                 
-                Button("Great") {
-                    messageString = "You Are Great!"
-                }  // Button
-                .buttonStyle(.borderedProminent)
-                           
-            }  // HStack
-            .border(.purple, width: 5)
-            .padding()
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.red)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+    //                .border(.orange, width: 5)
+                    .padding()
+                
+                Spacer()
+                
+                Divider()
+                    .background(.black)
+                    .padding()
+                    .frame(width: 200.0)
+                
+                Rectangle()
+                    .fill(.indigo)
+                    .frame(width: geometry.size.width * (0.75), height: 5)
+                
+                HStack {
+                
+                    Button("Awesome") {
+                        messageString = "You Are Awesome!"
+                    }  // Button
+                    .buttonStyle(.borderedProminent)
+                    
+                    Spacer()
+                    
+                    Button("Great") {
+                        messageString = "You Are Great!"
+                    }  // Button
+                    .buttonStyle(.borderedProminent)
+                               
+                }  // HStack
+    //            .border(.purple, width: 5)
+                .padding()
+                
+            }  // VStack
             
-        }  // VStack
+        }  // GeometryReader
         
     }
 }
