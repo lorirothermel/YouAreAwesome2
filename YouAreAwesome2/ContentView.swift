287df16b28c8a040fa33de72d4d53b9c19d2db21
lorirothermel <lori@rothermel1.com>
2024-05-12 23:17:23 -0400

    Lesson 10 Screen Design and color

--- YouAreAwesome2/ContentView.swift
+++ YouAreAwesome2/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = "When the Genius Bar needs help, they call you!"
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            
+//            Text("You Have Skills!")
+//                .font(.largeTitle)
+//                .fontWeight(.black)
+//                .foregroundColor(Color("Gold-BC"))
+//                .padding()
+//                .background(Color("Maroon-BC"))
+//                .padding(1)
+//                .cornerRadius(15)
+            
+            Spacer()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .border(.orange, width: 5)
+                .padding()
+            
+            Spacer()
+                       
+            HStack {
+            
+                Button("Awesome") {
+                    messageString = "You Are Awesome!"
+                }  // Button
+                .buttonStyle(.borderedProminent)
+                
+                Spacer()
+                
+                Button("Great") {
+                    messageString = "You Are Great!"
+                }  // Button
+                .buttonStyle(.borderedProminent)
+                           
+            }  // HStack
+            .border(.purple, width: 5)
+            .padding()
+            
+        }  // VStack
+        
     }
 }
 
