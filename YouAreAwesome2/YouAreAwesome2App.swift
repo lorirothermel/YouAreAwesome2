6ce02a483408eb307c393b6f0f809a246a041a8b
lorirothermel <lori@rothermel1.com>
2024-05-12 22:25:40 -0400

    Initial Commit

---
+++ YouAreAwesome2/YouAreAwesome2App.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesome2App.swift
+//  YouAreAwesome2
+//
+//  Created by Lori Rothermel on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesome2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
