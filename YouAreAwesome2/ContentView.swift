8693d9b14ce4b00a90856373c6fe925fbccdbe8f
lorirothermel <lori@rothermel1.com>
2024-05-13 22:22:52 -0400

    Chapter 1.5

--- YouAreAwesome2/ContentView.swift
+++ YouAreAwesome2/ContentView.swift
@@ -8,67 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = "When the Genius Bar needs help, they call you!"
+    @State private var messageString = ""
     
     var body: some View {
         
-        GeometryReader { geometry in
-            VStack {
-                
-    //            Text("You Have Skills!")
-    //                .font(.largeTitle)
-    //                .fontWeight(.black)
-    //                .foregroundColor(Color("Gold-BC"))
-    //                .padding()
-    //                .background(Color("Maroon-BC"))
-    //                .padding(1)
-    //                .cornerRadius(15)
-                
-                Spacer()
-                
-                Text(messageString)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .minimumScaleFactor(0.5)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.red)
-                    .frame(height: 150)
-                    .frame(maxWidth: .infinity)
-    //                .border(.orange, width: 5)
-                    .padding()
-                
-                Spacer()
-                
-                Divider()
-                    .background(.black)
-                    .padding()
-                    .frame(width: 200.0)
-                
-                Rectangle()
-                    .fill(.indigo)
-                    .frame(width: geometry.size.width * (0.75), height: 5)
-                
-                HStack {
-                
-                    Button("Awesome") {
-                        messageString = "You Are Awesome!"
-                    }  // Button
-                    .buttonStyle(.borderedProminent)
-                    
-                    Spacer()
-                    
-                    Button("Great") {
-                        messageString = "You Are Great!"
-                    }  // Button
-                    .buttonStyle(.borderedProminent)
-                               
-                }  // HStack
-    //            .border(.purple, width: 5)
-                .padding()
-                
-            }  // VStack
+        
+        VStack {
             
-        }  // GeometryReader
+            Spacer()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .padding()
+            
+            Spacer()
+            
+            HStack {
+                
+                Button("Awesome") {
+                    messageString = "You Are Awesome!"
+                }  // Button
+                .buttonStyle(.borderedProminent)
+                
+                Spacer()
+                
+                Button("Great") {
+                    messageString = "You Are Great!"
+                }  // Button
+                .buttonStyle(.borderedProminent)
+                
+            }  // HStack
+            .padding()
+            
+        }  // VStack
+        
+        
         
     }
 }
